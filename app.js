73dbae5e8b8dc663b4c04c51102d8587a52fe5df
Gerardo Jared <amayajared3@gmail.com>
2025-02-07 18:22:38 -0600

    Añadir funcionamiento a input, botón y mostrar en lista los nombres

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-let indicador = document.getElementById('asd');
-let listaNombre = []
+var numeros = "0123456789";
+let listaNombre = [];
 
-function agregarNombre() {
-    let divLista = document.getElementById('lista');
+
+function verificarEntrada(){
     let nombre = document.getElementById('entradaNombres').value;
-    let parrafoNombre = document.createElement('p');
-    parrafoNombre.innerHTML = nombre
-    divLista.appendChild(parrafoNombre);
-
-    return nombre;
+    if (nombre === "" || verificarNumeros(nombre) ==1){
+        alert("Escriba un nombre válido para agregarlo.")
+        document.getElementById('entradaNombres').value = "";
+    }
+    else {
+        listaNombre.push(nombre)
+        document.getElementById('entradaNombres').value = "";
+        mostrarNombreEnLista()
+        return nombre;
+    }
+    
 }
 
-console.log(agregarNombre());
+function mostrarNombreEnLista() {
+    let lista = document.getElementById('lista');
+    lista.innerHTML = "";
+    for (let i = 0; i< listaNombre.length; i++) {
+        let amigo = listaNombre[i];
+        let crearLista = document.createElement('li');
+        crearLista.textContent = amigo;
+
+        lista.appendChild(crearLista)
+
+    }
+}
+
+function verificarNumeros(texto){
+for (let i= 0; i < texto.length; i++) {
+    if (numeros.indexOf(texto.charAt(i), 0)!=-1) {
+        return true;
+    }
+}
+    return false; 
+
+}
