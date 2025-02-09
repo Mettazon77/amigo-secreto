849db285d9c820fdb7d691d22b2045ac5e7034c3
Gerardo Jared <amayajared3@gmail.com>
2025-02-08 20:11:14 -0600

    Mejora de sistema para validar nombres

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var numeros = "0123456789";
 let listaNombre = [];
 
 
+
 function verificarEntrada(){
     let nombre = document.getElementById('entradaNombres').value;
-    if (nombre === "" || verificarNumeros(nombre) ==1){
+    if (nombre === "" || verificarNumeros(nombre)){
         alert("Escriba un nombre válido para agregarlo.")
         document.getElementById('entradaNombres').value = "";
     }
@@ -12,9 +13,9 @@ function verificarEntrada(){
         listaNombre.push(nombre)
         document.getElementById('entradaNombres').value = "";
         mostrarNombreEnLista()
+        actualizarBotonElegir();
         return nombre;
     }
-    
 }
 
 function mostrarNombreEnLista() {
@@ -24,9 +25,10 @@ function mostrarNombreEnLista() {
         let amigo = listaNombre[i];
         let crearLista = document.createElement('li');
         crearLista.textContent = amigo;
-
+        
         lista.appendChild(crearLista)
-
+        console.log(listaNombre.length - 1)
+        
     }
 }
 
@@ -37,5 +39,26 @@ for (let i= 0; i < texto.length; i++) {
     }
 }
     return false; 
+}
 
+function elegirGanador() {
+    let numRandom = Math.floor(Math.random()* listaNombre.length);
+    console.log(numRandom)
+    let ganador = listaNombre[numRandom];
+    let titulo = document.getElementById('asd');
+
+
+    titulo.innerHTML = `¡${ganador} ES EL ELEGIDO!`;
+    console.log(listaNombre.length)
+    console.log(ganador);
+}
+
+function actualizarBotonElegir() {
+    let botonElegir = document.getElementById('ganador');
+
+    if (listaNombre.length > 1) {  
+        botonElegir.removeAttribute('disabled'); 
+    } else {
+        botonElegir.setAttribute('disabled', true); 
+    }
 }
